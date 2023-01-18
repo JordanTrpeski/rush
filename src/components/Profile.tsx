bcdac27420400aa1cd35b3514f52030263a76a01
Jordan Trpeski <viralstark@gmail.com>
2023-01-18 12:50:12 +0100

    Updated profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,56 +1,56 @@
 import { useState } from "react"
-export  function Profile() {
-  const [nameOpen, setNameOpen ] = useState(false)
-  const [lastnOpen, setlastnOpen ] = useState(false)
-  const [emailOpen, setemailOpen ] = useState(false)
-  const [numberOpen, setnumberOpen ] = useState(false)
-  const [passwordOpen, setpasswordOpen ] = useState(false)
 
-  return ( 
+function EditableField({ value, onValueChange, onValueSubmit }: { value: string, onValueSubmit: () => void, onValueChange: (value: string) => void }) {
+  const [open, setOpen] = useState(false)
+  return <div className="p-2 bg-white border shadow rounded w-96">
+    <div className="flex justify-between items-center">
+      {
+        open ? <input value={value} onChange={e => onValueChange(e.target.value)} className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">{value}</div>
+      }
+      <button type="button" onClick={() => { if (open) onValueSubmit(); setOpen(!open) }} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{open ? 'Save' : 'Edit'}</button>
+    </div>
+  </div >
+}
+
+function PasswordChange() {
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+
+  return <div>
+    <h4 className="text-xl mt-4 text-gray-900 font-bold">Password</h4>
+    <div className="p-2 bg-white border shadow rounded w-96">
+      <div className="flex flex-col">
+        <div>Old Password</div>
+        <input value={oldPassword} onChange={e => setOldPassword(e.target.value)} type="password" className="mb-2 border-solid border-2 border-gray-300 rounded" />
+        <div>New Password</div>
+        <input value={newPassword} onChange={e => setNewPassword(e.target.value)} type="password" className="mb-2 border-solid border-2 border-gray-300 rounded" />
+        <button type="button" className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">Change</button>
+      </div>
+    </div >
+  </div>
+}
+
+function PersonalInfo() {
+  const [firstName, setFirstName] = useState('Jontra')
+  const [lastName, setLastName] = useState('Volta')
+  const [email, setEmail] = useState('lesh@kari.al')
+  const [phoneNumber, setPhoneNumber] = useState('123512')
+
+  return <div>
+    <h4 className="text-xl text-gray-900 font-bold">Personal Info</h4>
+    <EditableField value={firstName} onValueChange={setFirstName} onValueSubmit={() => { }}></EditableField>
+    <EditableField value={lastName} onValueChange={setLastName} onValueSubmit={() => { }}></EditableField>
+    <EditableField value={email} onValueChange={setEmail} onValueSubmit={() => { }}></EditableField>
+    <EditableField value={phoneNumber} onValueChange={setPhoneNumber} onValueSubmit={() => { }}></EditableField>
+  </div>
+}
+
+export function Profile() {
+  return (
     <div>
       <div className="flex-1 bg-white rounded-lg shadow-xl p-8">
-        <h4 className="text-xl text-gray-900 font-bold">Personal Info</h4>
-        <div className="p-2 bg-white border shadow rounded w-96">
-          <div className="flex justify-between items-center">
-            {
-              nameOpen ? <input className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">Jontra</div>
-            }
-            <button type="button" onClick={() => setNameOpen(!nameOpen)} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{nameOpen ? 'Save' : 'Edit'}</button>
-          </div>
-        </div >
-        <div className="p-2 bg-white border shadow rounded w-96">
-          <div className="flex justify-between items-center">
-            {
-              lastnOpen ? <input className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">Voltra</div>
-            }
-            <button type="button" onClick={() => setlastnOpen(!lastnOpen)} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{lastnOpen ? 'Save' : 'Edit'}</button>
-          </div>
-        </div >
-        <div className="p-2 bg-white border shadow rounded w-96">
-          <div className="flex justify-between items-center">
-            {
-              emailOpen ? <input className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">emai@email.com</div>
-            }
-            <button type="button" onClick={() => setemailOpen(!emailOpen)} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{emailOpen ? 'Save' : 'Edit'}</button>
-          </div>
-        </div >
-        <div className="p-2 bg-white border shadow rounded w-96">
-          <div className="flex justify-between items-center">
-            {
-              numberOpen ? <input className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">Phone Number</div>
-            }
-            <button type="button" onClick={() => setnumberOpen(!numberOpen)} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{numberOpen ? 'Save' : 'Edit'}</button>
-          </div>
-        </div >
-        <div className="p-2 bg-white  border shadow rounded w-96">
-          <div className="flex justify-between items-center">
-            {
-              passwordOpen ? <input className="ml-2 border-solid border-2 border-gray-300 rounded" /> : <div className="ml-2">Password </div>
-            }
-            <button type="button" onClick={() => setpasswordOpen(!passwordOpen)} className="btn bg-gray-200 hover:bg-gray-300 px-4 py-2 font-medium rounded">{passwordOpen ? 'Save' : 'Edit'}</button>
-          </div>
-        </div >
-
+        <PersonalInfo />
+        <PasswordChange />
       </div>
     </div>
   )
